94a0976b2d240cf08de34a4a53944e8ebaa229f4
rreinold <rreinold@gmail.com>
2020-06-04 18:25:55 -0500

    Preprocessing done

--- train.py
+++ train.py
@@ -4,9 +4,8 @@ from os import listdir
 import glob
 import code
 
-DEBUG = False
+DEBUG = True
 
-# TODO Add more columns to this over time
 
 # The columns of interest are, for each joint of interest, 
 # the 3x3 submatrix below extracted from the 4x4
@@ -14,8 +13,14 @@ DEBUG = False
 # 2  6 10 -
 # 3  7 11 -
 # -  -  - -
+# TODO Add more columns to this over time
 COLUMNS_OF_INTEREST = ['hips_joint1','hips_joint2','hips_joint3','hips_joint5','hips_joint6','hips_joint7','hips_joint9','hips_joint10','hips_joint11']
+
+# This is the longest data capture, aka highest number of rows in all the csvs
+# TODO When tuning, reduce
+MAX_CAPTURED_SEQUENCE = 328
 CAPTURED_DATA_DIR = "./captured/*/*.csv"
+LABEL_DATA_DIR = "./captured/*/label.txt"
 
 # selected 3x3 submatrix, COLUMNS_OF_INTEREST, range from -1,1, so normalize to 0,1
 def normalize(csv):
@@ -32,17 +37,23 @@ def loadCapturedData():
 		x.append(normalized)
 	if DEBUG: 
 		print(x)
-		code.interact(local=locals())
 	return x
+
+def loadLabelData():
+	all_files = glob.glob(LABEL_DATA_DIR)
+	y = []
+	for f in all_files:
+		with open(f, 'r') as file:
+			label = file.read().replace('\n','')
+			y.append(label)
+	if DEBUG: 
+		print(y)
+	return y
 
 print("Filtering on the following columns: \n", "\n".join(COLUMNS_OF_INTEREST))
 
-df = loadCapturedData()
-
-# This is the longest data capture, aka highest number of rows in all the csvs
-# TODO When tuning, reduce
-max_captured_sequence = 328
-
+x = loadCapturedData()
+y = loadLabelData()
 
 
 if DEBUG:
