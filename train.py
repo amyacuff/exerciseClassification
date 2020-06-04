94ffdb826f35dbc6206c57981753744a972cd0d0
rreinold <rreinold@gmail.com>
2020-06-04 12:52:21 -0500

    normalized to 0,1

--- train.py
+++ train.py
@@ -7,9 +7,10 @@ csv = pd.read_csv(ACTIVITY_FILEPATH)
 # TODO Add more columns to this over time
 columnsOfInterest = csv[['hips_joint1','hips_joint2','hips_joint3','hips_joint5','hips_joint6','hips_joint7','hips_joint9','hips_joint10','hips_joint11']]
 
+columnsOfInterest += 1
+columnsOfInterest /= 2
+
 print(columnsOfInterest)
-
-
 
 import code
 code.interact(local=locals())
