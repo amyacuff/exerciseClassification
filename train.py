cea4f001018f51c067c8b4198ac110c1fc5a5708
rreinold <rreinold@gmail.com>
2020-06-05 16:22:03 -0500

    guarantee ordering of captured and input data

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from tensorflow.keras.preprocessing import sequence
+from sklearn.model_selection import train_test_split
 import pandas as pd
 from os import listdir
 import glob
@@ -6,6 +7,8 @@ import code
 
 DEBUG = True
 
+# % of dataset to allocate to training
+TRAINING_SPLIT = 0.6
 
 # The columns of interest are, for each joint of interest, 
 # the 3x3 submatrix below extracted from the 4x4
@@ -19,8 +22,13 @@ COLUMNS_OF_INTEREST = ['hips_joint1','hips_joint2','hips_joint3','hips_joint5','
 # This is the longest data capture, aka highest number of rows in all the csvs
 # TODO When tuning, reduce
 MAX_CAPTURED_SEQUENCE = 328
+
+CAPTURES_PER_EXERCISE = 10
+
 CAPTURED_DATA_DIR = "./captured/*/*.csv"
-LABEL_DATA_DIR = "./captured/*/label.txt"
+
+# TODO Ensure captured matches label
+LABEL_DATA_DIR = "./captured/*/*_label.txt"
 
 # selected 3x3 submatrix, COLUMNS_OF_INTEREST, range from -1,1, so normalize to 0,1
 def normalize(csv):
@@ -40,12 +48,13 @@ def loadCapturedData():
 	return x
 
 def loadLabelData():
-	all_files = glob.glob(LABEL_DATA_DIR)
+	all_files = sorted(glob.glob(LABEL_DATA_DIR))
 	y = []
 	for f in all_files:
 		with open(f, 'r') as file:
 			label = file.read().replace('\n','')
-			y.append(label)
+			for i in range(CAPTURES_PER_EXERCISE):
+				y.append(label)
 	if DEBUG: 
 		print(y)
 	return y
@@ -55,6 +64,13 @@ print("Filtering on the following columns: \n", "\n".join(COLUMNS_OF_INTEREST))
 x = loadCapturedData()
 y = loadLabelData()
 
+x = sequence.pad_sequences(x, maxlen=MAX_CAPTURED_SEQUENCE)
+
+
+# TODO Add shuffle
+x_train, x_test, y_train, y_test = train_test_split(x,y, train_size=TRAINING_SPLIT)
+
+print(x)
 
 if DEBUG:
 	code.interact(local=locals())
